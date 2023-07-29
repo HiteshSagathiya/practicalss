a618523049b9c67976bcc461476a1e025acf266c
HiteshSagathiya <138553460+HiteshSagathiya@users.noreply.github.com>
2023-07-29 23:35:31 +0530

    Add files via upload

---
+++ practical-1.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let str="awt"
+let b='true'
+let a=10
+
+console.log(str);
+console.log(b);
+console.log(a);
+
+const sum=(a,b)=>{
+    return a+b;
+}
+
+let x=15
+let y=20
+console.log(`the sum of two numbers is ${sum(15,20)}`);
+
